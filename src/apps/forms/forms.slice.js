9ed4552b83afaa96b47f142b44e96f6ecc74cd74
Mallikarjun <mallikarjunkamtam@gmail.com>
2022-10-23 21:29:51 +0530

    forms completed

--- src/apps/forms/forms.slice.js
+++ src/apps/forms/forms.slice.js
@@ -1,6 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {};
+const initialState = {
+  initialValues: {
+    firstName: "",
+    lastName: "",
+    email: "",
+    city: "",
+    sport: [{ id: "", name: "" }],
+    color: [{ id: "", name: "" }],
+  },
+};
 
 const formSlice = createSlice({
   name: "forms",
@@ -9,9 +18,14 @@ const formSlice = createSlice({
     forms: (state, action) => {
       return state;
     },
+    setInitialValues: (state, action) => {
+      state.initialValues = action.payload;
+      return state;
+    },
   },
 });
 
 export default formSlice.reducer;
 
 export const formActions = formSlice.actions;
+export const formState = (state) => state;
